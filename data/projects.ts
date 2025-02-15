67885f913786d0d3df6523a41a5033afd46db157
axelriv62 <axel_riviere@ens.univ-artois.fr>
2025-02-15 11:29:42 +0100

    :card_file_box: Ajout des données des réalisations

---
+++ data/projects.ts
@@ -0,0 +1,87 @@
+export type Projects = {
+    image: string;
+    title: string;
+    description: string;
+    tags: string[];
+    link: string;
+}
+
+export const projects: Projects[] = [
+    {
+        image: "/next.svg",
+        title: "Portfolio",
+        description: "Site personnel pour présenter mes compétences et mes réalisations.",
+        tags: ["Next.js", "Tailwind CSS", "TypeScript"],
+        link: "https://github.com/axelriv62/portfolio",
+    },
+    {
+        image: "/next.svg",
+        title: "ActiviteSport",
+        description: "Développement d'une API permettant la gestion de salles de sport.",
+        tags: ["Laravel", "API", "Postman", "SQLite"],
+        link: "https://github.com/axelriv62/activite-sport",
+    },
+    {
+        image: "/next.svg",
+        title: "VanLifer",
+        description: "Application de publication de voyages en van.",
+        tags: ["Laravel", "Blade", "CSS", "SQLite"],
+        link: "https://github.com/axelriv62/vanlifer",
+    },
+    {
+        image: "/next.svg",
+        title: "Lens Judge",
+        description: "Juge automatique pour les compétitions de programmation.",
+        tags: ["Java"],
+        link: "https://github.com/axelriv62/lens-judge",
+    },
+    {
+        image: "/next.svg",
+        title: "Bomberman",
+        description: "Jeu du Bomberman.",
+        tags: ["Java", "JavaFX"],
+        link: "https://github.com/axelriv62/bomberman",
+    },
+    {
+        image: "/next.svg",
+        title: "Serveur Personnel",
+        description: "Recyclage d'une machine en serveur multimédia.",
+        tags: ["Docker", "Unix", "Bash"],
+        link: "https://github.com/axelriv62/home-server",
+    },
+    {
+        image: "/next.svg",
+        title: "Les Recettes de Mamy Lens",
+        description: "Application de gestion de recette.",
+        tags: ["Laravel", "Blade", "CSS", "SQLite"],
+        link: "https://github.com/axelriv62/recettes-de-mamy-lens",
+    },
+    {
+        image: "/next.svg",
+        title: "Juste Le Prix",
+        description: "Jeu du Juste Prix.",
+        tags: ["Python", "Flask", "CSS", "SQLite"],
+        link: "https://github.com/axelriv62/juste-le-prix",
+    },
+    {
+        image: "/next.svg",
+        title: "2048",
+        description: "Jeu du 2048.",
+        tags: ["Java", "JavaFX"],
+        link: "https://github.com/axelriv62/2048",
+    },
+    {
+        image: "/next.svg",
+        title: "Puissance 4",
+        description: "Jeu du Puissance 4.",
+        tags: ["Java", "JavaFX"],
+        link: "https://github.com/axelriv62/four-in-a-line",
+    },
+    {
+        image: "/next.svg",
+        title: "Abyss Explore",
+        description: "Site vitrine d'une entreprise organisatrice de voyages de plongé.",
+        tags: ["HTML", "CSS"],
+        link: "https://github.com/axelriv62/abyss-explore",
+    },
+];
